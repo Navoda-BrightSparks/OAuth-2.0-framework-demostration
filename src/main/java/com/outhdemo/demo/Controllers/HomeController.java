b59435534441d511f0051eafe27061e092aa5983
navoda sathsarani <it15110244@my.sliit.lk>
2018-10-26 08:10:21 +0530

    redirect controller

---
+++ src/main/java/com/outhdemo/demo/Controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.outhdemo.demo.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@CrossOrigin(origins = { "http://localhost:9000" })
+public class HomeController {
+    @GetMapping("/home")
+    public String home() {
+
+        return "home";
+    }
+}
